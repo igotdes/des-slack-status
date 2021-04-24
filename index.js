0a65e28336c9f414c856a36f07784ec71fee1d9d
igotdes <74066241+igotdes@users.noreply.github.com>
2021-04-24 18:55:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const emojiRegex = require('emoji-regex');
 const nodeEmoji = require('node-emoji');
 const slack = require('slack');
 const moment = require('moment');
-
+var timer;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -21,54 +21,73 @@ app.post('/', (req, res, next) => {
     next();
     return;
   }
+  if ( 'undefined' !== typeof timer ) {
+   clearTimeout( timer );
+  }
+
   // store token
   const token = process.env.SLACK_TOKEN;
   // log some stuff for dev
   console.log(req.body);
   // grab status and emojis and clean it up
   let status = req.body.title;
-  let statusEmoji = nodeEmoji.unemojify('🗓');
-  const statusHasEmoji = emojiRegex().exec(status);
-  if (statusHasEmoji) {
-    statusEmoji = nodeEmoji.unemojify(statusHasEmoji[0]);
-    console.log(`CUSTOM EMOJI! ${statusEmoji}`);
-    status = nodeEmoji.strip(status);
-  }
   // additional tokens
-  const dndToken = '[DND]';
-  const awayToken = '[AWAY]';
+  const dndToken = 'Woo Chat';
   // parse event start/stop time
   const dateFormat = 'MMM D, YYYY [at] hh:mmA';
   const start = moment(req.body.start, dateFormat);
   const end = moment(req.body.end, dateFormat);
+  wizardStatusStartTime = end.diff(start, 'seconds') * 1000;
   // check for DND
   if (status.includes(dndToken)) {
+   console.log('Setting snooze');
+   console.log(status);
     slack.dnd.setSnooze({
       token,
-      num_minutes: end.diff(start, 'minutes')
+      num_minutes: end.diff(start, 'minutes') - 2
     });
-    status = status.replace(dndToken, '').trim();
   }
-  // check for AWAY
-  slack.users.setPresence({
-    token,
-    presence: status.includes(awayToken) ? 'away' : 'auto'
-  });
-  if (status.includes(awayToken)) {
-    status = status.replace(awayToken, '').trim();
+  var emoji = ':chat-green:'
+  if ( status.match('Reserve') ) {
+   emoji = ':happychat:';
+  }
+
+  if ( status.match('Tickets') ) {
+   emoji = ':zendesk2:';
+  }
+  if ( status.match('Escalated') ) {
+   emoji = ':ohana:';
   }
   // set status
-  status = `${status} from ${start.format('h:mm')} to ${end.format('h:mm a')} ${process.env.TIME_ZONE}`;
+  status = `${status} from ${start.format('h:mm')} to ${end.format('h:mm a')}`;
   let profile = JSON.stringify({
     "status_text": status,
-    "status_emoji": statusEmoji,
-    "status_expiration": end.unix()
+    "status_emoji": emoji,
+    "status_expiration": end.utcOffset(-10,true).unix()
   });
   console.log(profile);
   slack.users.profile.set({ token, profile });
-  console.log(`Status set as "${status}" and will expire at ${end.format('h:mm a')}`);
+  //console.log(`Status set as "${status}" and will expire at ${end.format('h:mm a')}`);
+  time = end.unix() + 30;
+  text = "shift over";
+  slack.reminders.add({token, text, time})
+  console.log('setting reminder to go red for ' + time);
   res.status(200);
   res.send('🤘');
+  //const end = moment(req.body.end, dateFormat);
+  console.log("scheduling wizard status for " + wizardStatusStartTime + " ms from now");
+  timer = setTimeout(function(){
+   status = "Solidarity!";
+   emoji = ':raised-fist:';
+   expiration = 0;
+   let profile = JSON.stringify({
+     "status_text": status,
+     "status_emoji": emoji,
+     "status_expiration": expiration
+   });
+   console.log(profile);
+   slack.users.profile.set({ token, profile });
+  }, wizardStatusStartTime );
 });
 
 app.get('/', (req, res, next) => {
