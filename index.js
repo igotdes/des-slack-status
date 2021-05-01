9059649c3e9f4c914237bff4a8a8a8137ea7775c
igotdes <74066241+igotdes@users.noreply.github.com>
2021-05-02 00:36:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ app.post('/', (req, res, next) => {
   const awayToken = '[AWAY]';
   // parse event start/stop time
   const dateFormat = 'MMM D, YYYY [at] hh:mmA';
-  const start = moment(req.body.start, dateFormat).subtract(8, 'hours');
-  const end = moment(req.body.end, dateFormat).subtract(8, 'hours');
+  const start = moment(req.body.start, dateFormat);
+  const end = moment(req.body.end, dateFormat);
   // check for DND
   if (status.includes(dndToken)) {
     slack.dnd.setSnooze({
